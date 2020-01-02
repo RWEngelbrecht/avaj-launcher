cd26910219647ebe0e24bc8ffaeffc74b81a77a3
rengelbr <rengelbr@student.wethinkcode.co.za>
2020-01-02 12:36:41 +0200

    subclass of Tower. changes weather. returns weather provided by WeatherProvider

--- src/simulator/WeatherTower.java
+++ src/simulator/WeatherTower.java
@@ -1,10 +1,11 @@
 package simulator;
 
 import aircraft.Coordinates;
+import weather.WeatherProvider;
 
 public class WeatherTower extends Tower{
 	public String getWeather(Coordinates coordinates) {
-
+		return (WeatherProvider.getProvider().getCurrentWeather(coordinates));
 	}
 
 	private void changeWeather() {
