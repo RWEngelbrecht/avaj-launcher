6c1ae146daee2d85ebca8925351c813ce21cc976
rengelbr <rengelbr@student.wethinkcode.co.za>
2020-01-02 12:28:17 +0200

    constructs aircraft of subtype helicopter

--- src/aircraft/AircraftFactory.java
+++ src/aircraft/AircraftFactory.java
@@ -3,7 +3,13 @@ package aircraft;
 import simulator.interfaces.Flyable;
 
 public class AircraftFactory {
-	public Flyable newAircraft(String type, int longitude, int latitude, int height) {
-		
+	public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
+		if (type.equalsIgnoreCase("Helicopter")) {
+			//instantiate coordinates
+			Coordinates coordinates = new Coordinates(longitude, latitude, height);
+			//instantiate aircraft
+			return (new Helicopter(name, coordinates));
+		}
+		return (null);
 	}
 }
