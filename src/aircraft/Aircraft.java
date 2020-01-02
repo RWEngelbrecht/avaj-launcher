772bd17a67386cc3406d4a11e9c239b38c0f40f7
rengelbr <rengelbr@student.wethinkcode.co.za>
2020-01-02 12:25:13 +0200

    constructor populates superclass Aircraft attributes

--- src/aircraft/Aircraft.java
+++ src/aircraft/Aircraft.java
@@ -4,13 +4,16 @@ public class Aircraft {
 	protected long id;
 	protected String name;
 	protected Coordinates coordinates;
-	private long idCounter;
+	private long idCounter = 0;
 
 	protected Aircraft(String name, Coordinates coordinates) {
-
+		this.idCounter = nextId();
+		this.id = this.idCounter;
+		this.name = name;
+		this.coordinates = coordinates;
 	}
 
 	private long nextId() {
-
+		return (++this.idCounter);
 	}
 }
