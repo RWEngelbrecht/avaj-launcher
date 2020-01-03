cab56f9a74dca0c1e57fed78aaf008e63ae5c3b7
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-03 11:48:17 +0200

    renaming source directory to avaj because java is weird

--- src/simulator/interfaces/Flyable.java
+++
@@ -1,8 +0,0 @@
-package simulator.interfaces;
-
-import simulator.WeatherTower;
-
-public interface Flyable {
-	public void updateConditions();
-	public void registerTower(WeatherTower WeatherTower);
-}
