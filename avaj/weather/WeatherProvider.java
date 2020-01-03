0953b6019c8beb85c86a7709cabc3aab40935df1
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-03 12:11:10 +0200

    might need to think of other way to generate weather

---
+++ avaj/weather/WeatherProvider.java
@@ -0,0 +1,32 @@
+package avaj.weather;
+
+import avaj.aircraft.Coordinates;
+
+public class WeatherProvider {
+	private static WeatherProvider weatherProvider = null;
+	private String[] weather = {"sun", "snow", "rain", "fog"};
+
+	private WeatherProvider() {
+
+	}
+
+	public static WeatherProvider getProvider() {
+		if (weatherProvider == null)
+			weatherProvider = new WeatherProvider();
+		return (weatherProvider);
+	}
+
+	public String getCurrentWeather(Coordinates coordinates) {
+		int height = coordinates.getHeight();
+		// int latidude = coordinates.getLatitude();
+		// int longitude = coordinates.getLongitude();
+
+		if (height <= 100 && height >= 90)
+			return (weather[0]);
+		else if (height < 90 && height >= 80)
+			return (weather[1]);
+		else if (height < 80 && height >= 70)
+			return (weather[2]);
+		return (weather[3]);
+	}
+}
