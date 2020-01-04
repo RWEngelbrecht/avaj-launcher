a0f52a3a94b7fc762eaf84b983826218312bb5d3
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-04 08:10:00 +0200

    prints to console. if remove height change, problem goes away. why?

--- avaj/aircraft/Baloon.java
+++ avaj/aircraft/Baloon.java
@@ -4,6 +4,7 @@ import avaj.simulator.*;
 import avaj.simulator.interfaces.*;
 
 public class Baloon extends Aircraft implements Flyable {
+
 	private WeatherTower weatherTower;
 
 	protected Baloon(String name, Coordinates coordinates) {
@@ -11,7 +12,8 @@ public class Baloon extends Aircraft implements Flyable {
 	}
 
 	public void updateConditions() {
-		String weather = weatherTower.getWeather(this.coordinates);
+System.out.printf("Baloon %s conditions updated  at longitude %d  latitude %d   height %d %n", this.name,this.coordinates.getLongitude(),this.coordinates.getLatitude(), this.coordinates.getHeight());
+		String weather = this.weatherTower.getWeather(this.coordinates);
 		if (weather.equalsIgnoreCase("sun")) {
 			this.coordinates = new Coordinates(
 				this.coordinates.getLongitude() + 2,
@@ -19,6 +21,7 @@ public class Baloon extends Aircraft implements Flyable {
 				this.coordinates.getHeight() + 4
 			);
 			Simulator.printWriter.println("Baloon#"+this.name+"("+this.id+"): Praise the sun!");
+			System.out.println("Baloon#"+this.name+"("+this.id+"): Praise the sun!");
 		} else if (weather.equalsIgnoreCase("rain")) {
 			this.coordinates = new Coordinates(
 				this.coordinates.getLongitude(),
@@ -26,6 +29,7 @@ public class Baloon extends Aircraft implements Flyable {
 				this.coordinates.getHeight() - 5
 			);
 			Simulator.printWriter.println("Baloon#"+this.name+"("+this.id+"): Raindrops keep falling on my head... Wait... *screams*");
+			System.out.println("Baloon#"+this.name+"("+this.id+"): Raindrops keep falling on my head... Wait... *screams*");
 		} else if (weather.equalsIgnoreCase("fog")) {
 			this.coordinates = new Coordinates(
 				this.coordinates.getLongitude(),
@@ -33,6 +37,7 @@ public class Baloon extends Aircraft implements Flyable {
 				this.coordinates.getHeight() - 3
 			);
 			Simulator.printWriter.println("Baloon#"+this.name+"("+this.id+"): You'd think the rotors would help clear this fog.");
+			System.out.println("Baloon#"+this.name+"("+this.id+"): You'd think the rotors would help clear this fog.");
 		} else if (weather.equalsIgnoreCase("snow")) {
 			this.coordinates = new Coordinates(
 				this.coordinates.getLongitude(),
@@ -40,12 +45,14 @@ public class Baloon extends Aircraft implements Flyable {
 				this.coordinates.getHeight() - 15
 			);
 			Simulator.printWriter.println("Baloon#"+this.name+"("+this.id+"): Staying frosty. As if we have a choice...");
+			System.out.println("Baloon#"+this.name+"("+this.id+"): Staying frosty. As if we have a choice...");
 		}
 		if (this.coordinates.getHeight() <= 0) {
 			Simulator.printWriter.println("Baloon#"+this.name+"("+this.id+") landing.");
 			this.weatherTower.unregister(this);
 			Simulator.printWriter.println("Tower says: Baloon#"+this.name+"("+this.id+") unregistered from weather tower.");
 		}
+System.out.printf("new long: %d%nnew lat: %d%nnew height: %d%n",this.coordinates.getLongitude(),this.coordinates.getLatitude(),this.coordinates.getHeight());
 	}
 
 	public void registerTower(WeatherTower weatherTower) {
