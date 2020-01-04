59a759ec8933806a0839a9dbf8627f6ac0bdcd5e
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-04 08:08:57 +0200

    also creates jetplanes and baloons. baloons giving problems

--- avaj/aircraft/AircraftFactory.java
+++ avaj/aircraft/AircraftFactory.java
@@ -6,9 +6,19 @@ public class AircraftFactory {
 	public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
 		if (type.equalsIgnoreCase("Helicopter")) {
 			//instantiate coordinates
+// System.out.println("is Helicopter");
 			Coordinates coordinates = new Coordinates(longitude, latitude, height);
 			//instantiate aircraft
 			return (new Helicopter(name, coordinates));
+		} else if (type.equalsIgnoreCase("JetPlane")) {
+// System.out.println("is jetplane");
+			Coordinates coordinates = new Coordinates(longitude, latitude, height);
+			return (new JetPlane(name, coordinates));
+		}
+		else if (type.equalsIgnoreCase("Baloon")) {
+// System.out.println("is baloon");
+			Coordinates coordinates = new Coordinates(longitude, latitude, height);
+			return (new Baloon(name, coordinates));
 		}
 		return (null);
 	}
