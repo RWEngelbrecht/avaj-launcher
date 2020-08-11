a462f3df5dc55872bb7c753c81317afa630299a4
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-08-11 17:20:58 +0200

    changed the way weather is generated

--- avaj/weather/WeatherProvider.java
+++ avaj/weather/WeatherProvider.java
@@ -1,11 +1,12 @@
 package avaj.weather;
 
 import avaj.aircraft.Coordinates;
+import java.util.Random;
 
 public class WeatherProvider {
 	private static WeatherProvider weatherProvider = null;
 	private String[] weather = {"sun", "snow", "rain", "fog"};
-
+	private Random rnd = new Random();
 	private WeatherProvider() {
 
 	}
@@ -16,8 +17,6 @@ public class WeatherProvider {
 		return (weatherProvider);
 	}
 
-
-//TODO: Find a way to use coordinates for weather production
 	public String getCurrentWeather(Coordinates coordinates) {
 		int height = coordinates.getHeight();
 		// int latidude = coordinates.getLatitude();
@@ -25,10 +24,13 @@ public class WeatherProvider {
 
 		if (height <= 100 && height >= 75)
 			return (weather[0]);
-		else if (height < 75 && height >= 55)
-			return (weather[1]);
-		else if (height < 55 && height >= 35)
-			return (weather[2]);
-		return (weather[3]);
+		else if (height < 75 && height >= 35) {
+			if ((rnd.nextInt() % 3) == 0)
+				return (weather[2]);
+			else if ((rnd.nextInt() % 2) == 0)
+				return (weather[1]);
+			return (weather[0]);
+		}
+		return (weather[rnd.nextInt(4)]);
 	}
 }
