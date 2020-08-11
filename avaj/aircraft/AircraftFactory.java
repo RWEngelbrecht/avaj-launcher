3ae90e74ed52c6dc55efd7545761ea74391b7d29
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-08-11 17:15:58 +0200

    cleaned up

--- avaj/aircraft/AircraftFactory.java
+++ avaj/aircraft/AircraftFactory.java
@@ -4,20 +4,12 @@ import avaj.simulator.interfaces.*;
 
 public class AircraftFactory {
 	public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
+		Coordinates coordinates = new Coordinates(longitude, latitude, height);
 		if (type.equalsIgnoreCase("Helicopter")) {
-			//instantiate coordinates
-// System.out.println("is Helicopter");
-			Coordinates coordinates = new Coordinates(longitude, latitude, height);
-			//instantiate aircraft
 			return (new Helicopter(name, coordinates));
 		} else if (type.equalsIgnoreCase("JetPlane")) {
-// System.out.println("is jetplane");
-			Coordinates coordinates = new Coordinates(longitude, latitude, height);
 			return (new JetPlane(name, coordinates));
-		}
-		else if (type.equalsIgnoreCase("Baloon")) {
-// System.out.println("is baloon");
-			Coordinates coordinates = new Coordinates(longitude, latitude, height);
+		} else if (type.equalsIgnoreCase("Baloon")) {
 			return (new Baloon(name, coordinates));
 		}
 		return (null);
