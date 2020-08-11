3cb96cce215cdd20e540abb553a879c73f6a4eaf
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-08-11 17:19:18 +0200

    renamed variable according to uml diagram

--- avaj/simulator/Tower.java
+++ avaj/simulator/Tower.java
@@ -5,19 +5,19 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import avaj.simulator.interfaces.*;
 
 public class Tower {
-	private CopyOnWriteArrayList<Flyable> crafts = new CopyOnWriteArrayList<Flyable>();
+	private CopyOnWriteArrayList<Flyable> observers = new CopyOnWriteArrayList<Flyable>();
 
 	public void register(Flyable flyable) {
-		crafts.add(flyable);
+		observers.add(flyable);
 	}
 
 	public void unregister(Flyable flyable) {
-		crafts.remove(flyable);
+		observers.remove(flyable);
 	}
 
 	protected void conditionsChanged() {
-		if (crafts.size() >= 0) {
-			for (Flyable flyable : crafts) {
+		if (observers.size() >= 0) {
+			for (Flyable flyable : observers) {
 				flyable.updateConditions();
 			}
 		}
